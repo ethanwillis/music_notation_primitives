42f020639b74c5293533d0a0919129c992d8bc1f
Ethan Willis <elwillis1@gmail.com>
2022-05-16 20:47:27 -0500

    Create index.js
    Basic implementation of a 5 line "staff" that can be rendered in a Euclid app.

---
+++ index.js
@@ -0,0 +1,27 @@
+
+// Visual Components
+const staff = (x, y, width, height, margin) => (ctx) => {
+  return {
+    drawFunc: ({x, y, width, height, margin}) => {
+      let staff_line_heights = [];
+      for(let i = 0; i < 5; i++) {
+        staff_line_heights.push( (Math.floor(height / 5) * i) + y )
+      }
+
+      staff_line_heights.forEach( (staff_line_height) => {
+        ctx.beginPath();
+        ctx.moveTo(x + margin.left, staff_line_height + margin.top);
+        ctx.lineTo(width - margin.right, staff_line_height + margin.top);
+        ctx.stroke()
+      });
+    },
+    params: {x, y, width, height, margin}
+  }
+}
+
+const MusicNotationPrimitives = {
+  staff
+}
+
+
+export default MusicNotationPrimitives
